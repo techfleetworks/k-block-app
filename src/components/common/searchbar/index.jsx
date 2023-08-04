66b1d4b20977d04e6364801d3a3d8d3a5c6d5e01
Jay Mehta <23277339+jaymehta20@users.noreply.github.com>
2023-08-04 22:31:23 +0530

    UI changes

--- src/components/common/searchbar/index.jsx
+++ src/components/common/searchbar/index.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Label } from "@/components/ui/label"
-import {
-    Command,
-    CommandInput,
-} from "@/components/ui/command"
+import React from 'react';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 
-import BgCube from '@/components/ui/bgcube-icon'
+const SearchBar = () => {
+  return (
+    <div className="flex justify-center pt-12">
+      <div className="flex flex-col w-2/4 justify-center items-center gap-2">
+        <Label
+          htmlFor="search"
+          className="text-white text-xl font-semibold self-start"
+        >
+          The Kadena Blockchain Explorer
+        </Label>
+        <Input
+          type="text"
+          id="text"
+          placeholder="Search by Address / Request Key / Account Name / Block / Token / Domain Name"
+          className="rounded-xl text-neutral-n-400"
+        />
+      </div>
+    </div>
+  );
+};
 
-const Searchbar = () => {
-    return (
-        <div className="flex justify-center bg-primary-b-500 w-screen">
-            <BgCube />
-            <div className="grid py-6 w-5/12 items-center gap-1.5">
-                <Label htmlFor="search" className="text-neutral-n-0">The Kadena Blockchain Explorer</Label>
-                <Command id="search" >
-                    <CommandInput placeholder="Search by Address / Request Key / Account Name / Block / Token / Domain Name" />
-                </Command>
-            </div>
-            <BgCube />
-        </div>
-
-    )
-}
-
-export default Searchbar;
+export default SearchBar;
