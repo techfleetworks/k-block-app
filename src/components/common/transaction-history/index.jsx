d96294200b3318852e11dc61a806c1e01cef6b59
John Jang <jscolbe9675@gmail.com>
2023-08-08 13:17:00 -0400

    transaction update

--- src/components/common/transaction-history/index.jsx
+++ src/components/common/transaction-history/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const TransactionHistory = () => {
   return (
-    <div className="bg-neutral-n-0 w-fit p-5 rounded-[8px] ">
+    <div className="bg-neutral-n-0 w-[375px] p-5 rounded-[8px] ">
       <h1>Transaction History</h1>
     </div>
   );
