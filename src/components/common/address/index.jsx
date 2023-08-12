8f3cf0a43a96c8930d6a5d5ad63482cf5629a7b7
Diana Jordan <dianacjordan@gmail.com>
2023-08-12 19:32:37 -0400

    ud qrcode and copy icons, add inner boxes

--- src/components/common/address/index.jsx
+++ src/components/common/address/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import copy from "@/assets/copy.svg";
+import qrcode from "@/assets/qrcode.svg";
 import Image from "next/image";
 
 const Address = () => {
@@ -7,9 +8,15 @@ const Address = () => {
     <div className="bg-neutral-n-0 flex-1 p-5 rounded-xl drop-shadow-md">
       <h4>Address      
       </h4>
-      <p className="text-xl font-bold ">0x18612b...ebb13</p>
-      <Image width={16} height={16} src={copy} alt="copy icon" />
-      
+      <p className="text-xl font-bold py-2 ">0x18612b...ebb13</p>
+      <div className="flex">
+      <Image width={24} height={24} src={copy} alt="copy icon" className="border neutral-n-300 rounded-md p-1 mr-2"/>
+      <Image width={24} height={24} src={qrcode} alt="copy icon" />
+      </div>
+      <div className="bg-neutral-n-100 mt-5 mb-2 p-2 rounded-md">jkhk
+      </div>
+      <div className="bg-neutral-n-100 p-2 rounded-md">jkhk
+      </div>
     </div>
   );
 };
--- src/pages/wallet-overview.js
+++ src/pages/wallet-overview.js
@@ -8,8 +8,8 @@ export default function WalletOverview() {
    return (
     <main className="">
       <Navbar />
-      <h2 className="mt-8 mb-7 my-7 px-3 md:px-12 text-[32px] font-semibold">Wallet Overview</h2>
-      <div className="flex flex-wrap gap-7 justify-start sm:justify-between px-3 md:px-12 my-7">
+      <h2 className="mt-12 mb-7  px-3 md:px-12 text-[32px] font-semibold">Wallet Overview</h2>
+      <div className="flex flex-wrap gap-7 justify-start sm:justify-between  px-3 md:px-12">
         <Address/>
       </div>
       
