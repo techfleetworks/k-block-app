8db3bef1984ad4ca803c0b4bec773737dc07d8ef
Diana Jordan <dianacjordan@gmail.com>
2023-08-11 23:25:50 -0400

    wallet-overview style

--- src/pages/wallet-overview.js
+++ src/pages/wallet-overview.js
@@ -8,7 +8,7 @@ export default function WalletOverview() {
    return (
     <main className="">
       <Navbar />
-      <h2 className="mt-8 mb-7 px-3 md:px-12 my-7">Wallet Overview</h2>
+      <h2 className="mt-8 mb-7 my-7 px-3 md:px-12 text-[32px] font-semibold">Wallet Overview</h2>
       <div className="flex flex-wrap gap-7 justify-start sm:justify-between px-3 md:px-12 my-7">
         <Address/>
       </div>
