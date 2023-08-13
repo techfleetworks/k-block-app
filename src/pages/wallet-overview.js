337afac729cdc7cbaa7fa0bdc6c15197e28dfb6a
Diana Jordan <dianacjordan@gmail.com>
2023-08-12 22:47:28 -0400

    add shadcn tabs

--- src/pages/wallet-overview.js
+++ src/pages/wallet-overview.js
@@ -1,25 +1,36 @@
 import {
-    Address,
-    Footer,
-    Navbar,
-    PortfolioBreakdown,
-    WalletValue,
-  } from '@/components/common';
+  Address,
+  Footer,
+  Navbar,
+  PortfolioBreakdown,
+  WalletValue,
+} from '@/components/common';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 
 export default function WalletOverview() {
-   return (
+  return (
     <main className="">
       <Navbar />
-      <h2 className="mt-12 mb-7  px-3 md:px-12 text-[32px] font-semibold">Wallet Overview</h2>
+      <h2 className="mt-12 mb-7 px-3 md:px-12 text-[32px] font-semibold">Wallet Overview</h2>
       <div className="flex flex-wrap gap-7 justify-start sm:justify-between  px-3 md:px-12">
-        <Address/>
+        <Address />
         <div className="flex flex-wrap gap-6 bg-neutral-n-0 p-5 rounded-xl grow drop-shadow-md">
-          <PortfolioBreakdown/>
+          <PortfolioBreakdown />
           <WalletValue />
         </div>
-
       </div>
-      
+      <Tabs defaultValue="account" className="mt-12 px-3 md:px-12 ">
+        <TabsList>
+          <TabsTrigger value="transactions">Transactions</TabsTrigger>
+          <TabsTrigger value="tokens">Tokens</TabsTrigger>
+          <TabsTrigger value="nfts">NFTs</TabsTrigger>
+        </TabsList>
+        <TabsContent value="transactions">placeholder1</TabsContent>
+        <TabsContent value="tokens">placeholder2</TabsContent>
+        <TabsContent value="nfts">placeholder3</TabsContent>
+      </Tabs>
+
       <Footer />
     </main>
   );
