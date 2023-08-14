fdf779fb30e0e45787df19496b6662c02a3ba8e4
Diana Jordan <dianacjordan@gmail.com>
2023-08-13 22:49:19 -0400

    tabs ui

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -23,7 +23,7 @@ const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-t-lg px-5 py-3 text-base font-medium text-neutral-n-500 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-neutral-n-0 data-[state=active]:text-primary-b-500 z-10 ",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-t-lg px-5 py-3 text-base font-medium text-neutral-n-500 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-neutral-n-0 data-[state=active]:text-primary-b-500  z-10 ",
       className
     )}
     {...props}
