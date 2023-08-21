f2063724452054c70d1b7d878b1e9a7c5bf78129
Elyse Newland <elysenewland@gmail.com>
2023-08-21 17:16:01 -0400

    Correct h2 className syntax error

--- src/components/common/nft-asset-overview/index.jsx
+++ src/components/common/nft-asset-overview/index.jsx
@@ -1,7 +1,7 @@
 export default function NftAssetOverview() {
   return (
     <div className="bg-neutral-n-0 p-5 border-solid drop-shadow-md rounded-md mt-6">
-      <h2 class="text-neutral-n-800 text-xl font-bold leading-6">Overview</h2>
+      <h2 className="text-neutral-n-800 text-xl font-bold leading-6">Overview</h2>
     </div>
   );
 }
