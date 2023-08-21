ba9663a34baf64b5915be3cb929ea2fb7bd46bd8
Elyse Newland <elysenewland@gmail.com>
2023-08-21 12:36:34 -0400

    Create nft-asset page

---
+++ src/pages/nft-asset.js
@@ -0,0 +1,16 @@
+import { Navbar } from "@/components/common";
+
+export default function NftAsset() {
+  return (
+    <div className="bg-inherit">
+      <header>
+        <Navbar />
+      </header>
+      <div>
+        <section>{/* Grid */}</section>
+        <section>{/* Tables */}</section>
+        <section>{/* Cards */}</section>
+      </div>
+    </div>
+  );
+}
