358fdea2ee61fa31e5e1b6c8f17f4fbdc0011e44
Jay Mehta <23277339+jaymehta20@users.noreply.github.com>
2023-08-30 17:47:23 +0530

    start with copy item feature

--- src/components/common/help/index.jsx
+++ src/components/common/help/index.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React from 'react';
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip";
-import Image from "next/image";
-import GreyQuestionMark from "@/assets/grey-question-mark.svg";
+  TooltipArrow,
+} from '@/components/ui/tooltip';
+import Image from 'next/image';
+import GreyQuestionMark from '@/assets/grey-question-mark.svg';
 
 const Help = ({ label, tooltipText }) => (
   <TooltipProvider delayDuration={100}>
     <Tooltip>
       <TooltipTrigger className="flex items-center gap-2">
-        <Image src={GreyQuestionMark} width={13} height={13} alt="question mark" />
+        <Image
+          src={GreyQuestionMark}
+          width={13}
+          height={13}
+          alt="question mark"
+        />
         <p className="text-neutral-n-800">{label}</p>
       </TooltipTrigger>
-      <TooltipContent>
+      <TooltipContent className="text-neutral-n-0 border-0 px-3 py-2 rounded-md">
+        <TooltipArrow className="" />
         <p>{tooltipText}</p>
       </TooltipContent>
     </Tooltip>
