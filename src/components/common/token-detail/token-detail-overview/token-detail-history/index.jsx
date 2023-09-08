57d68b39688de5146d0aab2edc8d87a8ecf2548d
John Jang <jscolbe9675@gmail.com>
2023-09-07 23:52:16 -0400

    updated line charts and date indicators

--- src/components/common/token-detail/token-detail-overview/token-detail-history/index.jsx
+++ src/components/common/token-detail/token-detail-overview/token-detail-history/index.jsx
@@ -1,56 +1,93 @@
-import React, { useEffect, useRef, useState } from "react";
-import Chart from "chart.js/auto";
+import DateButtons from "@/components/ui/date-buttons";
 import SocialChannels from "../social-channels";
+import React, { useEffect, useRef } from "react";
+
+// import Chart from "chart.js/auto";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 export default function PriceHistory() {
-  const [activeButton, setActiveButton] = useState("24H");
-  const activeColor =
-    "bg-primary-b-500 border border-neutral-n-250 text-xs text-neutral-n-0 px-2 py-2 rounded-sm";
-  const inActiveColor =
-    "bg-neutral-n-50 border border-neutral-n-250 text-xs text-n-800 px-2 py-2";
+  // const canvasEl = useRef(null);
+  // const colors = {
+  //   blue: {
+  //     default: "rgba(0, 120, 204, 1)",
+  //   },
+  // };
 
-  function toggleActive(id) {
-    setActiveButton(id);
-  }
+  // useEffect(() => {
+  //   const ctx = canvasEl.current.getContext("2d");
 
-  const canvasEl = useRef(null);
+  //   const weight = [
+  //     0.27, 0.24, 0.35, 0.28, 0.5, 0.6, 0.52, 0.6, 0.48, 0.75, 1.0,
+  //   ];
 
-  const colors = {
-    blue: {
-      default: "rgba(0, 120, 204, 1)",
+  //   const labels = ["", "12pm", "", "6pm", "", "12am", "", "6am", "", ""];
+  //   const data = {
+  //     labels: labels,
+  //     datasets: [
+  //       {
+  //         label: "measured in $",
+  //         data: weight,
+  //         fill: false,
+  //         borderColor: colors.blue.default,
+  //         lineTension: 0.2,
+  //       },
+  //     ],
+  //   };
+  //   const config = {
+  //     type: "line",
+  //     data: data,
+  //   };
+  //   const myLineChart = new Chart(ctx, config);
+
+  //   return function cleanup() {
+  //     myLineChart.destroy();
+  //   };
+  // });
+
+  const data = [
+    {
+      number: 0,
+      Change: 0.27,
     },
-  };
-
-  useEffect(() => {
-    const ctx = canvasEl.current.getContext("2d");
-
-    const weight = [
-      0.27, 0.24, 0.35, 0.28, 0.5, 0.6, 0.52, 0.6, 0.48, 0.75, 1.0,
-    ];
-
-    const labels = ["", "12pm", "", "6pm", "", "12am", "", "6am", "", ""];
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          label: "measured in $",
-          data: weight,
-          fill: false,
-          borderColor: colors.blue.default,
-          lineTension: 0.2,
-        },
-      ],
-    };
-    const config = {
-      type: "line",
-      data: data,
-    };
-    const myLineChart = new Chart(ctx, config);
-
-    return function cleanup() {
-      myLineChart.destroy();
-    };
-  });
+    {
+      name: "12pm",
+      Change: 0.24,
+      number: 0.25,
+    },
+    {
+      Change: 0.35,
+    },
+    {
+      name: "6pm",
+      Change: 0.28,
+    },
+    {
+      Change: 0.5,
+    },
+    {
+      name: "12am",
+      Change: 0.6,
+    },
+    {
+      Change: 0.52,
+    },
+    {
+      name: "6am",
+      Change: 0.6,
+    },
+    {
+      number: 1,
+      Change: 0.48,
+    },
+  ];
 
   return (
     <div className="block xl:ml-5 xl:mt-0 mt-5 xl:w-[46vw] w-[95vw] -z-50">
@@ -60,42 +97,35 @@ export default function PriceHistory() {
             <h1 className="text-neutral-n-700 text-sm font-medium">
               Price History
             </h1>
+            <span className="text-neutral-n-700 text-xs font-base">
+              (measured in $)
+            </span>
           </div>
-          <div>
-            <button
-              onClick={() => toggleActive("24H")}
-              className={activeButton === "24H" ? activeColor : inActiveColor}
-            >
-              24H
-            </button>
-            <button
-              onClick={() => toggleActive("7D")}
-              className={activeButton === "7D" ? activeColor : inActiveColor}
-            >
-              7D
-            </button>
-            <button
-              onClick={() => toggleActive("14D")}
-              className={activeButton === "14D" ? activeColor : inActiveColor}
-            >
-              14D
-            </button>
-            <button
-              onClick={() => toggleActive("30D")}
-              className={activeButton === "30D" ? activeColor : inActiveColor}
-            >
-              30D
-            </button>
-          </div>
+          <DateButtons />
         </header>
         <div className="px-5 xl:mb-5 pb-5">
-          <div className="PriceHistory">
+          <div className="xl:w-[43vw] overflow-x-auto md:overflow-auto">
+            <LineChart
+              width={620}
+              height={250}
+              margin={{ top: 20, left: -20 }}
+              data={data}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis dataKey="number" />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="Change" stroke="#8884d8" />
+            </LineChart>
+          </div>
+          {/* <div className="PriceHistory">
             <canvas
               ref={canvasEl}
               height={100}
               className="overflow-auto w-full"
             />
-          </div>
+          </div> */}
         </div>
       </div>
       <SocialChannels />
---
+++ src/components/ui/date-buttons.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export default function DateButtons() {
+  const [activeButton, setActiveButton] = useState("24H");
+  const activeColor =
+    "bg-primary-b-500 border border-neutral-n-250 text-xs text-neutral-n-0 px-2 py-1 rounded-sm";
+  const inActiveColor =
+    "bg-neutral-n-50 border border-neutral-n-250 text-xs text-n-800 px-2 py-1 cursor-pointer";
+
+  function toggleActive(id) {
+    setActiveButton(id);
+  }
+
+  return (
+    <div className="flex">
+      <button
+        onClick={() => toggleActive("24H")}
+        className={activeButton === "24H" ? activeColor : inActiveColor}
+      >
+        24H
+      </button>
+      <button
+        onClick={() => toggleActive("7D")}
+        className={activeButton === "7D" ? activeColor : inActiveColor}
+      >
+        7D
+      </button>
+      <button
+        onClick={() => toggleActive("14D")}
+        className={activeButton === "14D" ? activeColor : inActiveColor}
+      >
+        14D
+      </button>
+      <button
+        onClick={() => toggleActive("30D")}
+        className={activeButton === "30D" ? activeColor : inActiveColor}
+      >
+        30D
+      </button>
+    </div>
+  );
+}
