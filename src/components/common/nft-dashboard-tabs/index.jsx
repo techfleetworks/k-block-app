9cf2c53dd2a03f46ef50b7791cfc6b01ef66dde8
John Jang <jscolbe9675@gmail.com>
2023-09-11 13:40:19 -0400

    made route changes

---
+++ src/components/common/nft-dashboard-tabs/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import Trending from "./trending";
+import Upcoming from "./upcoming";
+import TopNFTCollections from "./top-nft-collection";
+import BoardTabsPagination from "./board-tabs-pagination";
+
+// I could not find a better way to tag the pagination only in one place but to use it multiple times.
+
+export default function BoardTabs(props) {
+  return (
+    <Tabs defaultValue="transactions" className="mt-12">
+      <TabsList>
+        <TabsTrigger value="transactions">{props.category1}</TabsTrigger>
+        <TabsTrigger value="tokens">{props.category2}</TabsTrigger>
+        <TabsTrigger value="nfts">{props.category3}</TabsTrigger>
+      </TabsList>
+      <TabsContent value="transactions" className="rounded-tl-none">
+        <Trending title="Trending NFT Collections" />
+        <BoardTabsPagination pageSize={10} />
+        {/* <p className="my-10">Pagination needs to go here</p> */}
+      </TabsContent>
+      <TabsContent value="tokens">
+        <Upcoming title="Upcoming Collections" />
+        <BoardTabsPagination pageSize={10} />
+      </TabsContent>
+      <TabsContent value="nfts">
+        <TopNFTCollections title="Top NFT Collections" />
+        <BoardTabsPagination
+          pageSize={10}
+          pageIndex={0}
+          pageCount={10}
+          onPageChange={null}
+          onPageSizeChange={null}
+        />
+      </TabsContent>
+    </Tabs>
+  );
+}
