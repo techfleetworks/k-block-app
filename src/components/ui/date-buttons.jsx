e4de74191454fde5644af115fa433a5931c0ec60
Elyse Newland <elysenewland@gmail.com>
2023-09-11 16:00:56 -0400

    Add charts to nft-collections analytics tab, update DateButton styles

--- src/components/ui/date-buttons.jsx
+++ src/components/ui/date-buttons.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export default function DateButtons() {
   const [activeButton, setActiveButton] = useState("24H");
   const activeColor =
-    "bg-primary-b-500 border border-neutral-n-250 text-xs text-neutral-n-0 px-2 py-1 rounded-sm";
+    "bg-primary-b-500 border border-neutral-n-250 text-xs text-neutral-n-0 px-2 py-1";
   const inActiveColor =
     "bg-neutral-n-50 border border-neutral-n-250 text-xs text-n-800 px-2 py-1 cursor-pointer";
 
@@ -12,10 +12,12 @@ export default function DateButtons() {
   }
 
   return (
-    <div className="flex">
+    <div className="flex mb-3">
       <button
         onClick={() => toggleActive("24H")}
-        className={activeButton === "24H" ? activeColor : inActiveColor}
+        className={`${
+          activeButton === "24H" ? activeColor : inActiveColor
+        } rounded-l-md`}
       >
         24H
       </button>
@@ -33,7 +35,9 @@ export default function DateButtons() {
       </button>
       <button
         onClick={() => toggleActive("30D")}
-        className={activeButton === "30D" ? activeColor : inActiveColor}
+        className={`${
+          activeButton === "30D" ? activeColor : inActiveColor
+        } rounded-r-md`}
       >
         30D
       </button>
